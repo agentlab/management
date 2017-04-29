42d152c3c31c95625d45e75733abaf780bd03410
Dmitriy Shishkin <shishkindimon@gmail.com>
2017-04-29 14:48:43 +0300

    Add example Module

---
+++ src/examples/org/hypergraphdb/app/examples/Module1.java
@@ -0,0 +1,28 @@
+package org.hypergraphdb.app.examples;
+
+import org.hypergraphdb.HyperGraph;
+import org.hypergraphdb.app.management.HGModule;
+
+public class Module1 extends HGModule {
+
+//	public void install(HyperGraph graph) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+	public void uninstall(HyperGraph graph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(HyperGraph graph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void reset(HyperGraph graph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
